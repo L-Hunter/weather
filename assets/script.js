2a7189bb28e5417c5d163e332ca838c04b938f48
TheButlerDidIt <labutler@gmail.com>
2020-03-09 11:42:51 -0400

    sets up project

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const city = document.querySelector('.city');
+const temperature = document.querySelector('.temperature');
+const weatherIcon = document.querySelector('.weatherIcon');
+const conditions = document.querySelector('.conditions');
+
