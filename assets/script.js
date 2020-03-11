f502813fddc60011be5a82dd4b41d049551cc4d5
TheButlerDidIt <labutler@gmail.com>
2020-03-10 22:30:28 -0400

    fleshes out api details

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,30 @@ const temperature = document.querySelector('.temperature');
 const weatherIcon = document.querySelector('.weatherIcon');
 const conditions = document.querySelector('.conditions');
 
+let lat = "39.9";
+let lon = "-75";
+let locationData = "https://www.metaweather.com/api/location/search/?lattlong=" + lat + "," + lon;
+console.log(locationData);
+
+// let woeid = "2471217" // x[0].woeid;
+// let city = x.title;
+// currentConditions = x.consolidated_weather[0].weather_state_name;
+// let minTemp = x.consolidated_weather[0].min_temp;
+// let maxTemp = x.consolidated_weather[0].max_temp;
+// let currentTemp = x.consolidated_weather[0].the_temp;
+
+function getLocation(){
+
+}
+
+function updateTemp(){
+
+}
+
+function updateImage(){
+
+}
+
+function updateConditions(){
+
+}
